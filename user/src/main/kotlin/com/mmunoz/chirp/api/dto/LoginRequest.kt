f2a6c3d8583bdab40b1e59a70877cdb7d45c240a
manuelrobertomunoz <mmroberto100@gmail.com>
2025-10-11 12:42:20 -0700

    prueba

--- user/src/main/kotlin/com/mmunoz/chirp/api/dto/LoginRequest.kt
+++ user/src/main/kotlin/com/mmunoz/chirp/api/dto/LoginRequest.kt
@@ -8,5 +8,6 @@ data class LoginRequest(
     @JsonProperty("email")
     val email: String,
     @JsonProperty("password")
-    val password: String
+    val password: String,
+    val valorPrueba: String
 )
